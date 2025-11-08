3b3b9f754e3a28bf6a48f396cd5361dbd2392166
Pietro Bondioli <email@pietrobondioli.com.br>
2025-11-08 20:30:14 -0300

    use nix to retrieve treesitter grammars

--- dotfiles/nvim/lua/plugins/disable.lua
+++ dotfiles/nvim/lua/plugins/disable.lua
@@ -1,3 +1,11 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false, event = "VeryLazy", keys = false },
+  {
+  "nvim-treesitter/nvim-treesitter",
+  build = false,
+  opts = {
+    auto_install = false,  -- Disable automatic installation
+    ensure_installed = {}, -- Don't auto-install any parsers
+  },
+}
 }
