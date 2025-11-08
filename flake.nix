773c9a12b10ce5c648c373e67b1332cd87193ac2
Pietro Bondioli <email@pietrobondioli.com.br>
2025-11-08 11:24:00 -0300

    initial commit :D

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+	description = "NixOS";
+
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, home-manager, ... }: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home-manager
+				{
+					home-manager = {
+						useGlobalPkgs = true;
+						useUserPackages = true;
+						users.pietro = import ./home.nix;
+						backupFileExtension = "backup";
+					};
+				}
+			];
+		};
+	};
+}
---
+++ home.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "pietro";
+	home.homeDirectory = "/home/pietro";
+	home.stateVersion = "25.05";
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			hi = "echo hello";
+		};
+	};
+
+  programs.niri = {
+    settings = {
+      outputs."HDML-A-1" = {
+        scale = 2.0;
+      };
+    };
+  };
+
+
+	home.packages = with pkgs; [
+    alacritty
+    fuzzel
+    firefox
+
+    _1password-cli
+    _1password-gui
+
+    # --- IDEs / Editors ---
+    vscode
+    neovim
+    #jetbrains.rider # For .NET (use rider-fhs for better compatibility)
+
+    # --- Common CLI Tools ---
+    ripgrep  # Fast search
+    fzf      # Fuzzy finder
+    htop     # Process monitor
+
+    # AI
+    github-copilot-cli
+
+    lazydocker
+    lazygit
+
+    # bin
+    nodejs_24
+    go
+    rustc
+    dotnetCorePackages.sdk_9_0_1xx-bin
+    dotnetCorePackages.aspnetcore_9_0-bin
+	];
+
+	programs.git = {
+    enable = true;
+    userName = "Pietro Bondioli";
+    userEmail = "email@pietrobondioli.com.br";
+  };
+
+  programs.zsh = {
+    enable = true;
+  };
+
+  # 3. AUTOMATIC DEV SHELLS (This is the magic!)
+  # This configures direnv to automatically load the
+  # dev shells defined in your project's flake.nix
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
+
