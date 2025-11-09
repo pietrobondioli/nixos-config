5a09838e167110b5a75fdbd34a2cb76d55934e48
Pietro Bondioli <email@pietrobondioli.com.br>
2025-11-09 12:18:11 -0300

    configure swayidle

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
     wtype
     swaybg
     swaylock
+    swayidle
 
     # Browsers
     firefox
@@ -367,6 +368,19 @@
         adjustment-method = "wayland";
       };
     };
+  };
+
+  services.swayidle = {
+    enable = true;
+    extraArgs = [ "-w" ];
+    events = [
+      { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
+      { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
+    ];
+    timeouts = [
+      { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -f"; }
+      { timeout = 600; command = "systemctl suspend"; resumeCommand = "${pkgs.swaylock}/bin/swaylock -f"; }
+    ];
   };
 }
 
