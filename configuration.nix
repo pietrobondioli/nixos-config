aca6733850faaaeac8913e31c2c4a13b8d9ee10b
Pietro Bondioli <email@pietrobondioli.com.br>
2025-11-11 13:09:51 -0300

    configure secondary drivers

--- configuration.nix
+++ configuration.nix
@@ -243,13 +243,43 @@
   };
 
   environment.etc = {
-      "1password/custom_allowed_browsers" = {
-        text = ''
-          zen
-        '';
-        mode = "0755";
-      };
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        zen
+      '';
+      mode = "0755";
     };
+  };
+
+  fileSystems = {
+    # Btrfs drive (sda1)
+    "/mnt/btrfs-drive" = {
+      device = "/dev/disk/by-uuid/49317738-381d-4e44-bfd6-8d0f3ce17b56";
+      fsType = "btrfs";
+      options = [ "defaults" "nofail" ];
+    };
+
+    # exFAT drive (sdb1) - good for Windows/Mac compatibility
+    "/mnt/exfat-drive" = {
+      device = "/dev/disk/by-uuid/7FEA-F9DB";
+      fsType = "exfat";
+      options = [ "defaults" "uid=1000" "gid=100" "umask=022" "nofail" ];
+    };
+
+    # ext4 drive (sdc1)
+    "/mnt/ext4-drive" = {
+      device = "/dev/disk/by-uuid/32bbee13-3044-4584-a387-cd2bd29c65a9";
+      fsType = "ext4";
+      options = [ "defaults" "nofail" ];
+    };
+
+    # Second NVMe drive (nvme1n1p2) - ext4 partition
+    "/mnt/nvme-extra" = {
+      device = "/dev/disk/by-uuid/f67a281a-1938-42d2-acfa-364981de89b1";
+      fsType = "ext4";
+      options = [ "defaults" "nofail" ];
+    };
+  };
 
   # You can also add it to login if desired
 
