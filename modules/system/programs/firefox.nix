ac7711ec5b995fd08e6c2c6e62c9f7ca596e5184
Pietro Bondioli <email@pietrobondioli.com.br>
2025-11-17 23:35:22 -0300

    add split view flag in firefox

--- modules/system/programs/firefox.nix
+++ modules/system/programs/firefox.nix
@@ -4,5 +4,10 @@
   programs.firefox = {
     enable = true;
     package = inputs.firefox-nightly.packages.${pkgs.system}.firefox-nightly-bin;
+    policies = {
+      Preferences = {
+        "browser.tabs.splitView.enabled" = true;
+      };
+    };
   };
 }
