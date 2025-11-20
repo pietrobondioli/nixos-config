4c4b17e64cccfd1995747b347a3672843911cf13
Pietro Bondioli <email@pietrobondioli.com.br>
2025-11-20 20:24:54 -0300

    better xdg aliases

--- modules/home/theming/gtk.nix
+++ modules/home/theming/gtk.nix
@@ -2,19 +2,22 @@
   gtk = {
     enable = true;
     gtk3.bookmarks = [
-      "file:///mnt/btrfs-drive Btrfs Drive"
-      "file:///mnt/exfat-drive ExFAT Drive"
-      "file:///mnt/ext4-drive Ext4 Drive"
-      "file:///mnt/nvme-extra NVMe Extra"
-      "file://${config.myDefaults.user.home}/work/syniti Syniti"
-      "file://${config.myDefaults.user.home}/personal/sandbox Sandbox"
-      "file://${config.myDefaults.user.home}/Downloads Downloads"
-      "file://${config.myDefaults.user.home}/Documents Documents"
-      "file://${config.myDefaults.user.home}/Pictures Pictures"
-      "file://${config.myDefaults.user.home}/Videos Videos"
-      "file://${config.myDefaults.user.home}/Music Music"
-      "file://${config.myDefaults.user.home}/Templates Templates"
+      "file://${config.myDefaults.user.home}/personal personal"
+      "file://${config.myDefaults.user.home}/work/syniti syniti"
+      ""
+      "file://${config.myDefaults.user.home}/Downloads downloads"
+      "file://${config.myDefaults.user.home}/Documents documents"
+      "file://${config.myDefaults.user.home}/Pictures pictures"
+      "file://${config.myDefaults.user.home}/Videos videos"
+      "file://${config.myDefaults.user.home}/Music music"
+      "file://${config.myDefaults.user.home}/Templates templates"
+      ""
+      "file:///mnt/btrfs-drive btrfs-drive"
+      "file:///mnt/exfat-drive exfat-drive"
+      "file:///mnt/ext4-drive ext4-drive"
+      "file:///mnt/nvme-extra nvme-extra"
     ];
+
     theme = { name = config.myDefaults.theme.gtk; package = pkgs.gnome-themes-extra; };
     iconTheme = { name = config.myDefaults.theme.icons; package = pkgs.adwaita-icon-theme; };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
