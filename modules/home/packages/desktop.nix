08c22172bb633881494def7ae0b9f468b9cc87fe
Pietro Bondioli <email@pietrobondioli.com.br>
2025-12-03 08:29:38 -0300

    add freecad

--- modules/home/packages/desktop.nix
+++ modules/home/packages/desktop.nix
@@ -32,5 +32,18 @@
     # Document & Image Viewers
     zathura
     swayimg
+
+    # Architecture & Design
+    (pkgs.symlinkJoin {
+      name = "FreeCAD";
+      paths = [ pkgs.freecad-wayland ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/FreeCAD \
+          --set __GLX_VENDOR_LIBRARY_NAME mesa \
+          --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json
+      '';
+      meta.mainProgram = "FreeCAD";
+    })
   ];
 }
