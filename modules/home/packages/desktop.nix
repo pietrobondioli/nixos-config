50383f42f4336833a2f7581bfcd2d68d7079c130
Pietro Bondioli <email@pietrobondioli.com.br>
2025-12-07 21:46:24 -0300

    fix warnings, add default zellij template back

--- modules/home/packages/desktop.nix
+++ modules/home/packages/desktop.nix
@@ -41,7 +41,7 @@
       postBuild = ''
         wrapProgram $out/bin/FreeCAD \
           --set __GLX_VENDOR_LIBRARY_NAME mesa \
-          --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json
+          --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json
       '';
       meta.mainProgram = "FreeCAD";
     })
--- modules/home/services/swayidle.nix
+++ modules/home/services/swayidle.nix
@@ -2,10 +2,10 @@
   services.swayidle = {
     enable = true;
     extraArgs = [ "-w" ];
-    events = [
-      { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
-      { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
-    ];
+    events = {
+      before-sleep = "${pkgs.swaylock}/bin/swaylock -f";
+      lock = "${pkgs.swaylock}/bin/swaylock -f";
+    };
     timeouts = [
       { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -f"; }
       { timeout = 600; command = "systemctl suspend"; resumeCommand = "${pkgs.swaylock}/bin/swaylock -f"; }
