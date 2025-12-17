efb3ba00eb67481908c131ff3713c9de8974f19c
Pietro Bondioli <email@pietrobondioli.com.br>
2025-12-17 14:23:01 -0300

    recording was not working, add wf-recorder

--- modules/home/packages/desktop.nix
+++ modules/home/packages/desktop.nix
@@ -11,7 +11,8 @@
     grim
     satty
     gpu-screen-recorder
-    wl-screenrec # using wl-screenrec instead of wf-recorder due to FFmpeg bug in wf-recorder 0.5.0
+    wl-screenrec
+    wf-recorder # fallback for NVIDIA VAAPI issues with wl-screenrec
 
     # Clipboard & Input
     wtype
