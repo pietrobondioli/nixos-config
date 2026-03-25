66d4495de9cf9724e8a014df71841d4971868bef
Pietro Bondioli <email@pietrobondioli.com.br>
2026-03-25 11:55:12 -0300

    change display config

--- hosts/desktop/defaults.nix
+++ hosts/desktop/defaults.nix
@@ -5,7 +5,7 @@
   config.myDefaults.system.hostname = lib.mkForce "desktop";
 
   config.myDefaults.display = lib.mkForce {
-    output = "HDMI-A-1";
+    output = "DP-2";
     resolution = "3840x2160@60.000";
     scale = 1.60;
   };
